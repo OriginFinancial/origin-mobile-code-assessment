7b8afe775778111c2d8153fdf0bf77bba730ec76
Paulo Pieczarka <paulopieczarka@gmail.com>
2024-07-22 13:26:47 -0300

    feat: add form handler

--- src/auth/auth.tsx
+++ src/auth/auth.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import { useState } from 'react';
 import { useNavigation } from '../app-stack';
 import { Button } from '../_components/button';
 import { TextInput } from '../_components/text-input';
@@ -9,9 +10,13 @@ import { Labeled } from '../_components/labeled';
 import { style } from './auth.style';
 
 export const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const { navigate } = useNavigation();
 
   const handleSignIn = () => {
+    console.log('Sign in with:', email, password);
     navigate('Transactions');
   };
 
@@ -20,10 +25,10 @@ export const Auth = () => {
       <Text style={style.heading}>Welcome back!</Text>
       <View style={style.card}>
         <Labeled title="Email" nativeID="email">
-          <TextInput placeholder="user@domain.com" />
+          <TextInput placeholder="user@domain.com" onChangeText={setEmail} />
         </Labeled>
         <Labeled title="Password" nativeID="password">
-          <TextInput placeholder="Password" />
+          <TextInput placeholder="Password" onChangeText={setPassword} />
         </Labeled>
         <Button onPress={handleSignIn}>Sign In</Button>
       </View>
